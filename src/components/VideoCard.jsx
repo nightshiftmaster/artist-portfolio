ed390222b0f42b0eeb12e75b952f742ec4497d0c
nightshiftmaster <nightshiftmaster@gmail.com>
2025-01-06 14:33:27 +0200

    autoplay video

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,22 +1,34 @@
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 
 const VideoCard = ({ item }) => {
   const { t } = useTranslation();
+  const [videoUrl, setVideoUrl] = useState(item.url);
+
+  const handleVideoClick = () => {
+    if (!videoUrl.includes("autoplay=1")) {
+      const autoplayUrl = videoUrl.includes("?")
+        ? `${videoUrl}&autoplay=1`
+        : `${videoUrl}?autoplay=1`;
+      setVideoUrl(autoplayUrl); // Заменяем URL видео, чтобы активировать автозапуск
+    }
+  };
+
   return (
-    <div className="video-item gallery__item ">
+    <div className="video-item gallery__item" onClick={handleVideoClick}>
       <div className="video-description">
         <h2 className="text-block__h">{t(`headers.video.${item.id}`)}</h2>
         <p className="text-block__p">{t(`paragraphs.video.${item.id}`)}</p>
       </div>
       <iframe
-        src={item.url}
+        src={videoUrl}
         className="video"
         scrolling="no"
         frameBorder="0"
-        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share "
-        script-src="none"
+        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
       ></iframe>
     </div>
   );
 };
+
 export default VideoCard;
