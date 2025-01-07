3756e5719638050ba0c474c462d0e1027b3ca1d5
nightshiftmaster <nightshiftmaster@gmail.com>
2025-01-07 16:41:00 +0200

    adding metadata

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,33 @@ import About from "./pages/About";
 import { Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import ScrollToTop from "./components/ScrollToTop";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
-    <div className="wrapper">
-      <ScrollToTop />
-      <Navbar />
-      <div className="content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/gallery" element={<Gallery />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-        <Footer />
+    <>
+      <Helmet>
+        <meta property="og:title" content="Vlad Violin Show" />
+        <meta property="og:description" content="All My Music In One Place" />
+        <meta property="og:image" content="../public/images/6.jpg" />
+        <meta property="og:url" content="https://www.yoursite.com" />
+        <meta property="og:type" content="website" />
+      </Helmet>
+
+      <div className="wrapper">
+        <ScrollToTop />
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+          <Footer />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
