3416de2724bef2a898709cb6df917d03e9ff5e56
nightshiftmaster <nightshiftmaster@gmail.com>
2025-01-08 22:13:26 +0200

    update home gsap

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -35,32 +35,36 @@ const Home = () => {
 
     const allItems = gsap.utils.toArray(".gallery-mobile .gallery__item");
 
-    // allItems.forEach((item) => {
-    //   gsap.fromTo(
-    //     item,
-    //     { x: -100, opacity: 0 },
-    //     {
-    //       x: -40,
-    //       opacity: 4,
-    //       start: "top bottom",
-    //       end: "top center",
-    //       scrollTrigger: { trigger: item, scrub: true },
-    //     }
-    //   );
-    //   gsap.fromTo(
-    //     item,
-    //     { opacity: 1 },
-    //     {
-    //       opacity: 0,
-    //       scrollTrigger: {
-    //         trigger: item,
-    //         start: "bottom center",
-    //         end: "bottom ",
-    //         scrub: true,
-    //       },
-    //     }
-    //   );
-    // });
+    allItems.forEach((item) => {
+      gsap.fromTo(
+        item,
+        { x: -100, opacity: 0 },
+        {
+          x: 0,
+          opacity: 4,
+          duration: 3,
+          ease: "power2.out",
+          start: "top bottom",
+          end: "top center",
+          scrollTrigger: { trigger: item, scrub: true },
+        }
+      );
+      gsap.fromTo(
+        item,
+        { opacity: 1 },
+        {
+          opacity: 0,
+
+          ease: "power2.out",
+          scrollTrigger: {
+            trigger: item,
+            start: "bottom center-=200",
+            end: "bottom ",
+            scrub: true,
+          },
+        }
+      );
+    });
 
     itemsLeft.forEach((item) => {
       gsap.fromTo(
