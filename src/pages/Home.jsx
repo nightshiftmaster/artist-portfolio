9704a6e0af88914c1eeca17816d53bed1c13ae9d
nightshiftmaster <nightshiftmaster@gmail.com>
2025-01-10 01:17:54 +0200

    update smoother

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,12 +7,9 @@ import VideoCard from "../components/VideoCard";
 import { useTranslation } from "react-i18next";
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
-
 const Home = () => {
   const { t } = useTranslation();
-
   useEffect(() => {
-    // Проверяем поддержку touch-устройств
     if (ScrollTrigger.isTouch !== 1) {
       ScrollSmoother.create({
         wrapper: ".wrapper",
@@ -20,121 +17,144 @@ const Home = () => {
         smooth: 1.5,
         effects: true,
       });
-
-      // Анимация заголовка
       gsap.fromTo(
         ".hero-section",
-        { opacity: 1 },
+        { opacity: 4 },
         {
           opacity: 0,
           scrollTrigger: {
             trigger: ".hero-section",
-            start: "top center",
-            end: "bottom top",
+            start: " center",
+            end: "1300",
             scrub: true,
           },
         }
       );
-
-      // Левая галерея
       const itemsLeft = gsap.utils.toArray(".gallery__left .gallery__item");
+
+      const itemsRight = gsap.utils.toArray(".gallery__right .gallery__item");
       itemsLeft.forEach((item) => {
         gsap.fromTo(
           item,
-          { x: -150, opacity: 0 },
+          { x: -100, opacity: 0 },
           {
             x: 0,
-            opacity: 1,
+            opacity: 4,
+            // start: "-850",
+            // end: "-100",
+            scrollTrigger: { trigger: item, scrub: true },
+          }
+        );
+        gsap.fromTo(
+          item,
+          { opacity: 4 },
+          {
+            opacity: 0.2,
+            start: "-150",
+            end: "-10",
             scrollTrigger: {
               trigger: item,
-              start: "top 90%",
-              end: "top 10%",
               scrub: true,
             },
           }
         );
       });
 
-      // Правая галерея
-      const itemsRight = gsap.utils.toArray(".gallery__right .gallery__item");
       itemsRight.forEach((item) => {
         gsap.fromTo(
           item,
-          { x: 150, opacity: 0 },
+          { x: 50, opacity: 0 },
           {
             x: 0,
-            opacity: 1,
-            scrollTrigger: {
-              trigger: item,
-              start: "top 90%",
-              end: "top 10%",
-              scrub: true,
-            },
+            opacity: 4,
+            // start: "-850",
+            // end: "-100",
+            scrollTrigger: { trigger: item, scrub: true },
           }
         );
-      });
-    } else {
-      // Для мобильных устройств отключаем ScrollSmoother
-      const allItems = gsap.utils.toArray(".gallery-mobile .gallery__item");
-      allItems.forEach((item) => {
         gsap.fromTo(
           item,
-          { x: -50, opacity: 0 },
+          { opacity: 4 },
           {
-            x: 0,
-            opacity: 1,
-            duration: 1.5,
-            ease: "power2.out",
+            opacity: 0.2,
+            // start: "-850",
+            // end: "-100",
             scrollTrigger: {
               trigger: item,
-              start: "top 90%",
-              end: "top 50%",
               scrub: true,
             },
           }
         );
       });
     }
-  }, []);
 
+    const allItems = gsap.utils.toArray(".gallery-mobile .gallery__item");
+
+    allItems.forEach((item) => {
+      gsap.fromTo(
+        item,
+        { x: -100, opacity: 0 },
+        {
+          x: 0,
+          opacity: 4,
+          start: "top bottom",
+          end: "top center",
+          scrollTrigger: { trigger: item, scrub: true },
+        }
+      );
+      gsap.fromTo(
+        item,
+        { opacity: 4 },
+        {
+          opacity: 0,
+
+          ease: "power2.out",
+          scrollTrigger: {
+            trigger: item,
+            start: "bottom center",
+            end: "bottom ",
+            scrub: true,
+          },
+        }
+      );
+    });
+  }, []);
   return (
-    <div className="wrapper">
-      <div className="content">
-        <header className="hero-section fade-in">
-          <img
-            data-speed=".6"
-            className="hero"
-            src="./images/hero3.png"
-            alt="main-image"
-          />
-          <div className="container fade-in">
-            <div data-speed=".75" className="main-header">
-              <h1 className="main-title">{t("headers.home_header")}</h1>
-              <p className="main-slogan puff-in-center">
-                {t("paragraphs.home_paragraph")}
-              </p>
-            </div>
+    <div>
+      <header className="hero-section fade-in">
+        <img
+          data-speed=".6"
+          className="hero"
+          src="./images/hero3.png"
+          alt="main-image"
+        />
+        <div className="container fade-in">
+          <div data-speed=".75" className="main-header">
+            <h1 className="main-title">{t("headers.home_header")}</h1>
+            <p className="main-slogan puff-in-center">
+              {t("paragraphs.home_paragraph")}
+            </p>
           </div>
-        </header>
-        <div className="portfolo">
-          <div className="container">
-            <main className="gallery">
-              <div data-speed=".9" className="gallery__left">
-                {videoItems.slice(0, 4).map((item, i) => (
-                  <VideoCard key={i} item={item} index={i} />
-                ))}
-              </div>
-              <div data-speed="1.1" className="gallery__right">
-                {videoItems.slice(4, 8).map((item, i) => (
-                  <VideoCard key={i} item={item} index={i} />
-                ))}
-              </div>
-            </main>
-            <div className="gallery-mobile">
-              {videoItems.map((item, i) => (
-                <VideoCard key={i} item={item} />
-              ))}
+        </div>
+      </header>
+      <div className="portfolo">
+        <div className="container">
+          <main className="gallery">
+            <div data-speed=".9" className="gallery__left">
+              {videoItems.slice(0, 4).map((item, i) => {
+                return <VideoCard key={i} item={item} index={i} />;
+              })}
             </div>
+            <div data-speed="1.1" className="gallery__right">
+              {videoItems.slice(4, 8).map((item, i) => {
+                return <VideoCard key={i} item={item} index={i} />;
+              })}
+            </div>
+          </main>
+          <div className="gallery-mobile">
+            {videoItems.map((item, i) => {
+              return <VideoCard key={i} item={item} />;
+            })}
           </div>
         </div>
       </div>
